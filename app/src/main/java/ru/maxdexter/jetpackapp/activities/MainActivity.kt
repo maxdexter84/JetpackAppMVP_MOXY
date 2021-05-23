0b4c2418ba0e0beea37ffdaaa2fc1342b3a37bc6
maxdexter <Maximusnew84!>
2021-05-23 11:57:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/maxdexter/jetpackapp/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.maxdexter.jetpackapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.maxdexter.jetpackapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
