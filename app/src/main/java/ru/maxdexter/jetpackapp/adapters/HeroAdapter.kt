9c8e956d9fae63cc4e290299abb68fa2150e675a
maxdexter <Maximusnew84!>
2021-06-02 09:17:47 +0300

    added item_hero.xml

--- app/src/main/java/ru/maxdexter/jetpackapp/adapters/HeroAdapter.kt
+++ app/src/main/java/ru/maxdexter/jetpackapp/adapters/HeroAdapter.kt
@@ -4,12 +4,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import ru.maxdexter.jetpackapp.models.Hero
 
-class HeroAdapter: ListAdapter<Hero, HeroViewHolder>(DiffHero()) {
+class HeroAdapter : ListAdapter<Hero, HeroViewHolder>(DiffHero()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
         return HeroViewHolder.create(parent)
     }
 
     override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
-       holder.bind(getItem(position))
+        holder.bind(getItem(position))
     }
 }
